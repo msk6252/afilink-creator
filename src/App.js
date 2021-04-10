c106317eec607ab0808872cb20a8c1b7e1e6b34d
msk6252 <msk6252@yahoo.co.jp>
2021-04-10 15:53:50 +0900

    add: js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
+  function createHTML() {
+    const image = document.getElementById("image").value
+    const name = document.getElementById("name").value
+    const desc = document.getElementById("desc").value
+    const amazon = document.getElementById("amazon").value
+    const rakuten = document.getElementById("rakuten").value
+    const yahoo = document.getElementById("yahoo").value
+    var template = `<div class="af-box">
+  <div class="af-imgbox">
+    ${image}
+  </div>
+  <div class="af-textbox">
+    <div class="af-title">
+      ${name}
+    </div>
+  <div class="af-desc">
+    ${desc}
+  </div>
+  <br/ >
+  <div class="af-kobox af-amazon">
+    <a href="${amazon}" target="_blank" class="aflink" rel="noopener noreferrer">Amazon</a>
+  </div>
+  <div class="af-kobox af-rakuten">
+    <a href="${rakuten}" target="_blank" class="aflink" rel="noopener noreferrer">楽天</a>
+  </div>
+  <div class="af-kobox af-yahoo">
+    <a href="${yahoo}" target="_blank" class="aflink" rel="noopener noreferrer">Yahoo!</a>
+  </div>
+ </div>
+ <div class="clear"></div>
+</div>`
+    return template
+  }
+
+  function createLink() {
+    document.getElementById("result").value = createHTML();
+    console.log(createHTML());
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Affiliate Link Generator
       </header>
+      <div className="container">
+        画像：<input id="image"></input>
+      </div>
+      <div className="container">
+        商品名：<input id="name"></input>
+      </div>
+      <div className="container">
+        商品説明：<input id="desc"></input>
+      </div>
+      <div className="container">
+        Amazon：<input id="amazon"></input>
+      </div>
+      <div className="container">
+        楽天：<input id="rakuten"></input>
+      </div>
+      <div className="container">
+        Yahoo：<input id="yahoo"></input>
+      </div>
+      <div className="container">
+        <button onClick={createLink}>リンク生成</button>
+      </div>
+      <div className="container">
+        出力：<textarea cols="50" rows="30" id="result"></textarea>
+      </div>
     </div>
   );
 }
